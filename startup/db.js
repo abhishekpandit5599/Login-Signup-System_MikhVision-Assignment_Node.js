084dc933589f925a01ec45804509a2da0d892797
Abhishek Sharma <abhishekpandit5599@gmail.com>
2022-08-03 18:27:17 +0530

    Assignment Done

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 module.exports = () => {
    // mongdb cloud connection is here
   mongoose
-    .connect("mongodb://localhost/databaseName", {
+    .connect("mongodb://localhost:27017/Mikhvision_Assignment_DB", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
